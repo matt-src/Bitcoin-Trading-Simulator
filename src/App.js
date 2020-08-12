dec61f931bd421020133cf4e7cabe145741d1bad
tofurocks <mattwall0@gmail.com>
2020-08-11 19:03:49 -0700

    Change useState() to useReducer(), add changed-amount from price to previous price
    useReducer() works better than useState() for the use case because we have some logic that we need to handle upon updates
    Remove a bunch of console logs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import { getPriceFromApi } from './helpers/Prices';
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -14,23 +13,40 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 650, //TODO: make this bigger so the table can't get cut off
   },
 });
 
 function App() {
 
-  const [shares, setShares] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [amount, setAmount] = useState(1);
-  const [data, setData] = useState({}); //Bitcoin price information
-  const [lastPrice, setLastPrice] = useState(0); //Bitcoin price information
-  const [realized, setRealized] = useState(0); //Realized profit/loss
+  const TRADE_EXECUTE = 'TRADE_EXECUTE';
+  const AMOUNT_CHANGE = 'AMOUNT_CHANGE';
+  const PRICE_UPDATE = 'PRICE_UPDATE';
+
+  const initialState = {
+    shares: 0, //Position size in BTC
+    entry: 0, //Average entry price of position
+    amount: 1, //Amount to buy/sell upon trade
+    price: 0, //Current BTC price in USD
+    prevPrice: 0, //Previous BTC price in USD
+    change: 0, //Change from previous price to current
+    realized: 0 //Realized profit on position
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState); //Difference from last price
 
   const classes = useStyles();
 
+  // Create our number formatter.
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
   useEffect(() => {
+    //Do initial price update
     updatePrice().then(r => r); //Need to use 'then' here instead of await, since we aren't in an async function
+
     const interval = setInterval(async () => {
       await updatePrice();
     }, 10 * 1000);
@@ -39,74 +55,82 @@ function App() {
     }
   }, []);
 
-  const updatePrice = async () => {
-    let price = await getPriceFromApi();
-    if(data){
-      console.log("updating price, current data:")
-      console.log(data);
-      console.log("new data:");
-      console.log(price);
+  function reducer(state, action) {
+    switch (action.type) {
+      case TRADE_EXECUTE:
+        let tradeAmount = action.tradeAmount;
+        let newShares = state.shares + parseInt(tradeAmount);
+        let newEntry = state.entry;
+        if (tradeAmount > 0) { //Update average price if this is a BUY order
+          let rate = parseFloat(state.price);
+          let valueOfCurrentHoldings = (state.shares * state.entry);
+          let valueOfNewHoldings = tradeAmount * rate;
+          let totalAmount = state.shares + tradeAmount;
+          newEntry = (valueOfCurrentHoldings + valueOfNewHoldings) / totalAmount;
+        }
+        return {
+          ...state,
+          shares: newShares,
+          entry: newEntry
+        };
+      case AMOUNT_CHANGE:
+        return {
+          ...state,
+          amount: action.value
+        }
+      case PRICE_UPDATE:
+        let prevPrice = state.price; //Current price becomes previous price
+        if (state.price == 0) { //This is our first price update, so set previous price to current price
+          prevPrice = action.price;
+        }
+        return {
+          ...state,
+          prevPrice: prevPrice,
+          price: action.price,
+          change: action.price - prevPrice
+        }
+      default:
+        throw new Error();
     }
-    setData(price);
+  }
+
+  const updatePrice = async () => {
+    let newPriceData = await getPriceFromApi();
+    dispatch({ type: PRICE_UPDATE, price: newPriceData.data.USD.last })
   };
 
   const buyShares = () => {
-    console.log("buying " + amount);
-    tradeShares(amount);
+    tradeShares(state.amount);
   };
 
-  // Create our number formatter.
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  });
-
   const sellShares = () => {
-    let newAmount = shares - amount;
+    let newAmount = state.shares - state.amount;
     if (newAmount < 0) {
-      console.log("can't have less than 0 shares");
+      console.log("can't have less than 0 shares"); //TODO: display this message on the page instead of just in the console
       return;
     }
-    tradeShares(amount * -1);
+    tradeShares(state.amount * -1);
     //Update realized PNL
-    console.log("average: " + average);
-    console.log("price: " + getPrice());
-    setRealized(realized + (amount * (getPrice() - average)));
+    state.realized = (state.realized + (state.amount * (state.price - state.entry)));
   };
 
   const tradeShares = (tradeAmount) => {
-    let newShares = shares + parseInt(tradeAmount);
-    if (tradeAmount > 0) { //Update average price if this is a BUY order
-      let rate = parseFloat(getPrice());
-      let valueOfCurrentHoldings = (shares * average);
-      let valueOfNewHoldings = tradeAmount * rate;
-      let totalAmount = shares + tradeAmount;
-      let newAverage = (valueOfCurrentHoldings + valueOfNewHoldings) / totalAmount;
-      setAverage(newAverage);
-    }
-    setShares(newShares);
+    dispatch({ type: TRADE_EXECUTE, tradeAmount: parseInt(tradeAmount) })
   };
 
   const handleAmountChange = (e) => {
-    setAmount(parseInt(e.target.value));
-  };
-
-  const getPrice = () => {
-    try {
-      return data.data.USD.last;
-    } catch (e) {
-      return "0";
-    }
+    dispatch({ type: AMOUNT_CHANGE, value: e.target.value });
   };
 
   return (
     <div className="App">
       <h1>Rekt Simulator</h1>
-      <h2>Change {lastPrice ? lastPrice : <p>not loaded</p>}</h2>
-  <h2>BTC {data ? getPrice() : <p>-</p>} USD <Button onClick={updatePrice} variant="contained" color="primary">Update</Button></h2>
+      <h2>BTC {state.price ? formatter.format(state.price) : <p>-</p>} USD <Button onClick={updatePrice} variant="contained" color="primary">Update</Button></h2>
+      <h2>Previous: {formatter.format(state.prevPrice)}</h2>
+      <h2>Change: {formatter.format(state.change)}</h2>
       <Button variant="contained" color="primary" onClick={buyShares}>Buy</Button>
       <Button variant="contained" color="secondary" onClick={sellShares}>Sell</Button>
-      
+
       <label htmlFor="amount">Amount</label>
       <input
         id="amount"
@@ -115,30 +139,30 @@ function App() {
         min="1"
         max="1000"
         placeholder="1"
-        value={amount}
+        value={state.amount}
         onChange={handleAmountChange}
       />
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="caption table">
-        <caption>Position information</caption>
-        <TableHead>
-          <TableRow>
-            <TableCell>Position size</TableCell>
-            <TableCell align="right">Position value</TableCell>
-            <TableCell align="right">Average Price</TableCell>
-            <TableCell align="right">P/L unrealized</TableCell>
-            <TableCell align="right">P/L realized</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        <TableRow>
-          <TableCell component="th">{shares} BTC</TableCell>
-          <TableCell align="right">{data ? (formatter.format(parseFloat(getPrice()) * shares)) : <p>-</p>}</TableCell>
-          <TableCell align="right">{formatter.format(average)}</TableCell>
-          <TableCell align="right">{formatter.format((parseFloat(getPrice()) - average) * shares)}</TableCell>
-          <TableCell align="right">{formatter.format(realized)}</TableCell>
-        </TableRow>
-        </TableBody>
+          <caption>Position information</caption>
+          <TableHead>
+            <TableRow>
+              <TableCell>Position size</TableCell>
+              <TableCell align="right">Position value</TableCell>
+              <TableCell align="right">Entry Price</TableCell>
+              <TableCell align="right">P/L unrealized</TableCell>
+              <TableCell align="right">P/L realized</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell component="th">{state.shares} BTC</TableCell>
+              <TableCell align="right">{state.price ? (formatter.format(parseFloat(state.price) * state.shares)) : <p>-</p>}</TableCell>
+              <TableCell align="right">{formatter.format(state.entry)}</TableCell>
+              <TableCell align="right">{formatter.format((parseFloat(state.price) - state.entry) * state.shares)}</TableCell>
+              <TableCell align="right">{formatter.format(state.realized)}</TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
