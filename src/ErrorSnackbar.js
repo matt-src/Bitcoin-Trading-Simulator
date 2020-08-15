525fffb8ca58c9b4acc7a84025ebe7819e87742d
tofurocks <mattwall0@gmail.com>
2020-08-14 21:10:06 -0700

    Remove unused styling from ErrorSnackbar

--- src/ErrorSnackbar.js
+++ src/ErrorSnackbar.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
-    },
-}));
 
 export default function CustomizedSnackbars(props) {
     return (
