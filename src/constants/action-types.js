a12f05120db701b127b1c056e84740b8fd24b62d
tofurocks <mattwall0@gmail.com>
2020-08-17 00:49:23 -0700

    Add action functions, add action type constants
    Move action types from App.js into action-types.js
    Create action functions for actions we will need to dispatch

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,3 +1,7 @@
 //TODO: put real action types here
 
-export const ADD_ARTICLE = "ADD_ARTICLE";
+export const TRADE_EXECUTE = 'TRADE_EXECUTE';
+export const AMOUNT_CHANGE = 'AMOUNT_CHANGE';
+export const PRICE_UPDATE = 'PRICE_UPDATE';
+export const SNACKBAR_CLOSE = 'SNACKBAR_CLOSE';
+export const SNACKBAR_OPEN = 'SNACKBAR_OPEN';
