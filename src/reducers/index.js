6d135a615d54eb0f74adaed8693174ea5374ab86
tofurocks <mattwall0@gmail.com>
2020-08-19 18:10:36 -0700

    Fix bugs accessing data in reducer
    Replaced action.price usage with action.payload to reflect new action creator format
    Multiply tradeAmount by -1 when calculating realized PNL on a trade to get correct profit/loss amount
    Parse TRADE_EXECUTE's payload as an integer to avoid bug where value is calculated as a string (resulting in 10 + 10  = 1010, etc.)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,11 +16,13 @@ const initialState = {
 function rootReducer(state = initialState, action) {
     console.log("action:");
     console.log(action);
+    console.log("state:")
+    console.log(state);
 
     switch (action.type) {
 
         case TRADE_EXECUTE:
-            let tradeAmount = action.payload;
+            let tradeAmount = parseInt(action.payload);
             let newShares = state.shares + parseInt(tradeAmount);
             let newEntry = state.entry;
             let newRealized = state.realized; //Will only change if this is a sell
@@ -31,6 +33,7 @@ function rootReducer(state = initialState, action) {
                 let valueOfCurrentHoldings = (state.shares * state.entry);
                 let valueOfNewHoldings = tradeAmount * rate;
                 let totalAmount = state.shares + tradeAmount;
+                console.log("totalAmount is " + totalAmount);
                 newEntry = (valueOfCurrentHoldings + valueOfNewHoldings) / totalAmount;
             }
 
@@ -45,7 +48,7 @@ function rootReducer(state = initialState, action) {
                     return;
                 }*/
                 //Update realized PNL and balance
-                let realizedChange = tradeAmount * (state.price - state.entry);
+                let realizedChange = (tradeAmount * -1) * (state.price - state.entry);
                 newRealized = state.realized + realizedChange;
                 newBalance = state.balance + realizedChange;
             }
@@ -58,15 +61,15 @@ function rootReducer(state = initialState, action) {
                 balance: newBalance //This will only be changed if this is a sell order
             };
         case PRICE_UPDATE:
-            if (action.price == state.price) {
+            if (action.payload == state.price) {
                 return state; //No price change, don't update anything
             }
             let prevPrice = state.price; //Current price becomes previous price
             if (state.price == 0) { //This is our first price update, so set previous price to current price
-                prevPrice = action.price;
+                prevPrice = action.payload;
             }
             //Calculate adjusted balance and check if we are liquidated
-            let unrealized = (parseFloat(action.price) - state.entry) * state.shares;
+            let unrealized = (parseFloat(action.payload) - state.entry) * state.shares;
             if (state.balance + unrealized <= 0) {
                 console.log("rekt"); //TODO: rekt logic
             }
@@ -74,8 +77,8 @@ function rootReducer(state = initialState, action) {
             return {
                 ...state,
                 prevPrice: prevPrice,
-                price: action.price,
-                change: action.price - prevPrice
+                price: action.payload,
+                change: action.payload - prevPrice
             }
         case SNACKBAR_CLOSE:
             return {
